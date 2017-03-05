b8a8620782af6d950b43af3ce392fd3f38a91c90
Andy-Rong <727346595@qq.com>
2017-03-05 12:14:49 +0800

    添加好友列表的关闭按钮，调整右上角menu按钮布局，并更换图标

--- client/www/mock/websocketClient/template.js
+++ client/www/mock/websocketClient/template.js
@@ -2,10 +2,18 @@
  * Created by Andy on 2017/3/4.
  */
 (function () {
+	window.global = window.global ? window.global : {};
+	window.global.hideList = function(){
+		document.getElementById("friendFrame").style.display = "none";
+		document.getElementById("headBarRight").style.display = "table";
+	};
+
 	var innerHtml = [
 	'<h2 style="height:3rem;line-height:200%;text-align:center;background:#008eff;color:#fff">基友列表(在线)</h2>' +
 	'<ul id="friendList" style="width:100%;height:100%;position: absolute;padding-top:3rem;box-sizing: border-box;top:0;left:0"></ul>',
-		'<button>关闭</button>'
+	'<button onclick="global.hideList()" style="width:2rem;height:2rem;position:absolute;top:0;right:0;border:none;background: none;outline: none">' +
+	'<i class="triangle-icon"></i>' +
+	'</button>'
 	].join("");
 
 	var div = document.createElement("div");
@@ -17,6 +25,7 @@
 	div.style.right = "1rem";
 	div.style.top = "1rem";
 	div.style.zIndex = "990";
+	div.style.overflow = "hidden";
 	div.style.background = "rgba(250,250,250,0.7)";
 	div.style.boxShadow = "3px 3px 3px rgba(0,0,0,.7)";
 	div.id = "friendFrame";
