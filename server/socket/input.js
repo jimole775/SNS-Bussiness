c06f4d20bad0880c4a1aa3c6963b3f25e5375519
Andy-Rong <727346595@qq.com>
2017-03-15 01:59:01 +0800

    websocket do modules

--- server/socket/input.js
+++ server/socket/input.js
@@ -1,3 +1,98 @@
 /**
  * Created by Andy on 2017/3/14.
  */
+(function () {
+    var WebSocket = require("./init.js");
+    var key;
+    var mask = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+    WebSocket.prototype.run = function () {
+        var that = this;
+        require('net').createServer(function (socket) {
+            console.log("服务器net会话：", socket);
+            socket.on("connect", function (sock) {
+                console.log("connect:", sock);
+            });
+            socket.on('error', function (sock) {
+                console.log("error：", sock);
+            });
+            socket.on('lookup', function (sock) {
+                console.log("lookup：", sock);
+            });
+            socket.on('timeout', function (sock) {
+                console.log("timeout：", sock);
+            });
+            //socket.on('drain',function(sock){
+            //	console.log("drain：",sock);
+            //});
+            socket.on('data', function (e) {
+                var frame = that.tool.decodeDataFrame(e);
+                //第一次握手
+                if (frame.FIN === 0) {
+                    that.handshake(socket, e);
+                }
+                //数据交互
+                else {
+                    that.dataHost(socket, frame);
+                }
+            });
+
+        }).listen(81, function () {
+        });
+    };
+
+    WebSocket.prototype.dataHost = function (socket, frame) {
+        var that = this;
+        switch (frame.Opcode) {
+            case 8:
+                console.log("会话已经结束:", socket, frame.PayloadData);
+                break;
+            default :
+                that.opcode = frame.Opcode;
+                var data = JSON.parse(frame.PayloadData.toString()) || "";
+                switch (data.status) {
+                    case 0:   //连接,绑定用户名
+                        //如果map里面没有此用户，就存储session，并绑定用户名
+                        that.distributeUid(data, socket);
+                        break;
+                    case 1:   //协助通道的询问
+                        that.handleRMTRequest(data, socket);
+                        break;
+                    case 2:   //协助通道的应答
+                        that.handleRMTResponse(data, socket);
+                        break;
+                    case 3:   //远程协助交互通道
+                        that.RMTInterActive(data);
+                        break;
+                    case 4:   //断开协助通道
+
+                        break;
+                    case 5:   //关闭ws
+                        that.close(data);
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    default :
+                        break;
+                }
+                break;
+        }
+    };
+
+
+    var crypto = require('crypto');
+    //单个用户的握手实例;
+    WebSocket.prototype.handshake = function (socket, e) {
+        var original = e.toString().match(/Sec-WebSocket-Key: (.+)/)[1];
+        key = crypto.createHash("sha1").update(original + mask).digest("base64");
+        socket.write("HTTP/1.1 101 Switching Protocols\r\n");
+        socket.write("Upgrade:Websocket\r\n");
+        socket.write("Connection:Upgrade\r\n");
+        socket.write("Sec-WebSocket-Accept:" + key + "\r\n");
+        socket.write("\r\n");
+    };
+
+    module.exports = WebSocket;
+})();
--- server/socket/output.js
+++ server/socket/output.js
@@ -1,10 +1,147 @@
 /**
- * Created by Andy on 2017/3/14.
+ * Created by Andy on 2017/1/23.
  */
-//var WebSocket = require("./server/socket/socket.js");
+(function () {
+    var WebSocket = require("./input.js");
 
-//module.exports = WebSocket;
-var WebSocket = global.WebSocket;
-WebSocket.prototype.console = function(){
-	console.log("OK");
-};
+    var clients = [];
+    var namesMap = [];
+
+    //所有以创建连接的用户以{asker:session,assiant:session}的方式存储
+    var chanelMap = [];
+    WebSocket.prototype.send = function (socket, status, data) {
+        var that = this;
+        var emitProtocolMap = {
+            status: status,
+            items: data
+        };
+
+        var PayloadData = that.opcode == 1 ? JSON.stringify(emitProtocolMap) : new Buffer(JSON.stringify(emitProtocolMap));
+
+        socket.write(
+            that.tool.encodeDataFrame({
+                FIN: 1,
+                Opcode: that.opcode,
+                PayloadData: PayloadData
+            })
+        );
+    };
+
+    WebSocket.prototype.disconnectChanel = function (data) {
+        //如果是协助者的断开讯号,
+        var that = this;
+        var asker = null;
+        var helper = null;
+        chanelMap.forEach(function (item, index) {
+            if (item.asker.uid == data.uid || item.helper.uid == data.uid) {
+                asker = item.asker.session;
+                helper = item.helper.session;
+                that.send(asker, 4, {disconnect: true});
+                that.send(helper, 4, {disconnect: true});
+                chanelMap.splice(index, 1);   //删除远程会话通道
+            }
+        });
+
+    };
+
+    //两个用户的对接
+    WebSocket.prototype.openChanel = function (data) {
+        var that = this;
+        var asker = that.tool.getSession(data.uid, clients);
+        var helper = that.tool.getSession(data.items.helper, clients);
+
+        //存储远程对话通道
+        chanelMap.push({
+            asker: {uid: data.uid, session: asker},
+            helper: {uid: data.items.helper, session: helper}
+        });
+
+        that.send(asker, 2, {remoteRole: 1});
+        that.send(helper, 2, {remoteRole: 2});
+    };
+
+    WebSocket.prototype.RMTInterActive = function (data) {
+        var that = this;
+        var map = that.tool.getChanelSession(chanelMap,data.uid);
+        if (data.items.remoteRole == 1) {
+            that.send(map.helper, 3, data.items);
+        }
+        else if (data.items.remoteRole == 2) {
+            that.send(map.asker, 3, data.items);
+        }
+    };
+
+    WebSocket.prototype.close = function (data) {
+        this.refreshUserList(data);
+        this.disconnectChanel(data);
+    };
+
+    WebSocket.prototype.refreshUserList = function (data) {
+        var that = this;
+        //删除断线的用户，重新推送到客户端
+        clients.forEach(function (item, index) {
+            if (item.uid === data.uid) {
+                clients[index].session.destroy();
+                console.log("删除对象：", clients[index]);
+                clients.splice(index, 1);
+                namesMap.splice(index, 1);
+            }
+        });
+
+        clients.forEach(function (item, index) {
+            that.send(item.session, 0, {userList: namesMap.join(",")});
+        });
+    };
+
+    WebSocket.prototype.distributeUid = function (data, socket) {
+
+        if (!namesMap.join(",").match(new RegExp(data.uid, "g"))) {
+
+            var that = this;
+            clients.push({
+                uid: data.uid,
+                session: socket
+            });
+            namesMap.push(data.uid);
+
+            //向所有的用户推送用户名
+            clients.forEach(function (item, index) {
+                that.send(item.session, 0, {userList: namesMap.join(",")});
+            });
+
+        }
+    };
+
+    WebSocket.prototype.handleRMTRequest = function (data, socket) {
+        var helper = this.tool.getSession(data.items.helperUid, clients);
+        this.send(
+            helper,
+            1,
+            {
+                helper: data.items.helperUid,
+                RMTResponse: data.items.RMTResponse
+            }
+        );
+    };
+
+    WebSocket.prototype.handleRMTResponse = function (data, socket) {
+
+        if (data.items.RMTResponse) {
+            this.openChanel(data);
+        }
+        else {
+
+            var asker = this.tool.getSession(data.uid, clients);
+            this.send(
+                asker,
+                2,
+                {
+                    RMTResponse: data.items.RMTResponse
+                }
+            );
+
+        }
+    };
+    module.exports = WebSocket;
+
+})();
